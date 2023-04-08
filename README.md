7ebb075961ee8293ae5aab4913e48cebb5dd096c
jprice <jprice@izeni.com>
2023-04-07 20:47:02 -0600

    Change database user, password, and database name to PROJECT_NAME

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # node-template
 
-## Usage
+## Quickstart
 ```bash
 bash <(wget -qO- https://raw.githubusercontent.com/shakedown-street/node-template/main/startproject.sh) <project_name>
 ```
--- src/db.ts
+++ src/db.ts
@@ -1,14 +1,11 @@
 import { Pool } from 'pg';
 
 export const pool = new Pool({
-  user: 'pork',
-  password: 'pork',
-  database: 'node_ts_api',
+  user: 'PROJECT_NAME',
+  password: 'PROJECT_NAME',
+  database: 'PROJECT_NAME',
   host: 'localhost',
   port: 5433,
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
 });
 
 export const query = async (text: string, params: any[] = []) => {
