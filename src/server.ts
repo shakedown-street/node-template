13d795dc9a1609911044f67845cc1be02ff21402
jprice <jprice@izeni.com>
2023-04-07 19:55:31 -0600

    Rename server to app in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,52 +1,27 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import { WebSocketServer } from 'ws';
 
 import { authRouter } from './features/auth/routes';
 import { authMiddleware } from './features/auth/middleware';
 
 // Start express server
 
-const server = express();
+const app = express();
 
-server.use(cors());
+app.use(cors());
 
-server.use(bodyParser.json());
-server.use(
+app.use(bodyParser.json());
+app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 );
 
-server.use(authMiddleware);
+app.use(authMiddleware);
 
-server.use('/api/auth', authRouter());
+app.use('/api/auth', authRouter());
 
-server.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
-
-// Start websocket server
-
-const wss = new WebSocketServer({ port: 3030 });
-
-wss.on('listening', () => {
-  console.log('WebSocketServer listening on port 3030');
-});
-
-wss.on('connection', (ws, req) => {
-  console.log(`Client connected`);
-
-  ws.on('error', console.error);
-
-  ws.on('message', (message) => {
-    console.log(`Received message => ${message}`);
-  });
-
-  ws.on('close', () => {
-    console.log(`Client disconnected`);
-  });
-
-  ws.send(`Message from server`);
+app.listen(3000, () => {
+  console.log('Express listening on http://localhost:3000');
 });
